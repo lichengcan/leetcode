6599bd84f3247d03e4a84ac8a9f8f64e8f66faab
lichengcan <li929825769@163.com>
2023-09-19 17:44:56 +0800

    删除有序数组中的重复项2

--- src/test/java/com/example/leetcode/mians/删除有序数组中的重复项2.java
+++ src/test/java/com/example/leetcode/mians/删除有序数组中的重复项2.java
@@ -11,25 +11,27 @@ public class 删除有序数组中的重复项2 {
         //两个指针
         //每次拿两个元素进行添加，新拿的两个元素和之前的后面一个是否相同，相同则跳过
         int skip = 2;
-       return removeDup(nums,skip);
+        return removeDup(nums, skip);
     }
 
     /**
      * 保留允许连续重复次数内的元素
      * 从而实现了在不创建新数组的情况下删除重复项
+     *
      * @param nums
      * @param k
      * @return
      */
     private int removeDup(int[] nums, int k) {
         //当前指针
-        int cur = 0;
-        for (int x : nums) {
-            //cur < k ，如果k=2，代表前面2个元素无条件保留
-            //nums[cur-k]保留元素中最右边的 和当前元素 不相等：进行保留
-            if (cur < k || nums[cur - k] != x)
-                nums[cur++] = x;
+        int res = 0;
+        //cur < k ，如果k=2，代表前面2个元素无条件保留
+        //nums[cur-k]保留元素中最右边的 和当前元素 不相等：进行保留
+        for (int num : nums) {
+            if (res < k || nums[res - k] != num) {
+                nums[res++] = num;
+            }
         }
-        return cur;
+        return res;
     }
 }
