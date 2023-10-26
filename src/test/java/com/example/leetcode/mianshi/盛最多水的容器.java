d006864645a4550c4e60f54b72174b31260c73c3
lichengcan <li929825769@163.com>
2023-10-26 14:40:37 +0800

    盛最多水的容器

---
+++ src/test/java/com/example/leetcode/mianshi/盛最多水的容器.java
@@ -0,0 +1,40 @@
+package com.example.leetcode.mianshi;
+
+/**
+ * @author: lichengcan
+ * @date: 2023-10-26 14:30
+ * @description
+ **/
+public class 盛最多水的容器 {
+
+    //给定一个长度为 n 的整数数组 height 。有 n 条垂线，第 i 条线的两个端点是 (i, 0) 和 (i, height[i]) 。
+    //
+    //找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+    //
+    //返回容器可以储存的最大水量。
+    //
+    //说明：你不能倾斜容器。
+    //
+    //
+
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length-1;
+
+        int sum = 0;
+        while (left<right){
+            int tem = height[left]>height[right]?height[right]*(right-left):height[left]*(right-left);
+            if (tem>sum) {
+                sum=tem;
+            }
+            else {
+                if(height[left]>height[right]){
+                    right--;
+                }else{
+                    left++;
+                }
+            }
+        }
+        return sum;
+    }
+}
