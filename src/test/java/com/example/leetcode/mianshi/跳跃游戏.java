4fd1318dd1bd3e66b07c4ade388ca8bc9237f457
lichengcan <li929825769@163.com>
2023-11-29 10:50:42 +0800

    跳跃游戏

--- src/test/java/com/example/leetcode/mianshi/跳跃游戏.java
+++ src/test/java/com/example/leetcode/mianshi/跳跃游戏.java
@@ -7,10 +7,6 @@ package com.example.leetcode.mianshi;
  **/
 public class 跳跃游戏 {
 
-    //给你一个非负整数数组 nums ，你最初位于数组的 第一个下标 。数组中的每个元素代表你在该位置可以跳跃的最大长度。
-    //
-    //判断你是否能够到达最后一个下标，如果可以，返回 true ；否则，返回 false 。
-
 
     public boolean canJump(int[] nums) {
         int k = 0;
@@ -23,6 +19,12 @@ public class 跳跃游戏 {
         return true;
     }
 
+
+    //给你一个非负整数数组 nums ，你最初位于数组的 第一个下标 。数组中的每个元素代表你在该位置可以跳跃的最大长度。
+    //
+    //判断你是否能够到达最后一个下标，如果可以，返回 true ；否则，返回 false 。
+
+
     public static void main(String[] args) {
 
         跳跃游戏 test = new 跳跃游戏();
@@ -30,21 +32,19 @@ public class 跳跃游戏 {
         int[] ints1 = new int[]{3, 2, 1, 0, 4};
         System.out.println(test.canJump(ints));
         System.out.println(test.canJump1(ints));
+        System.out.println(test.canJump(ints1));
         System.out.println(test.canJump1(ints1));
     }
 
     public boolean canJump1(int[] nums) {
-        int k = 0;
-        //为什么i<=k
-        for (int i = 0; i <= k; i++) {
-            //当前能跳的距离
-            int temp = i + nums[i];
-            //更新跳的最远
-            k = Math.max(k, temp);
-            //如果最远>=最后一个元素的下标
-            if (k >= nums.length - 1) return true;
+        final int max = nums.length;
+        int maxSteps = 0;
+        int tem = nums[0]+1;
+        for (int i = 0; i < tem&&maxSteps<max; i++) {
+            maxSteps = Math.max(maxSteps, i + 1 + nums[i]);
+            tem=maxSteps;
         }
-        return false;
+        return maxSteps >= max;
     }
 
 }
